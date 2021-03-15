7e9c638726eaf57e9145108a81e7eaddd24af8f2
Anuj Mathur <anuj.mathur@rewardle.com>
2021-03-15 10:44:14 +0000

    Added authentication our project

---
+++ src/store/reducers/auth.js
@@ -0,0 +1,13 @@
+export * as actionTypes from "../actions/actionTypes"
+const initialState = {
+
+}
+const reducer = (state=initialState,action)=>{
+    switch(action.type){
+        case actionTypes.AUTH_START:
+            return{
+
+            }
+ 
+    }
+}
