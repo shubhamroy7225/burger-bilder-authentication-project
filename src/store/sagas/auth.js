b6c15497b6e20344f441f89f5ded9e00652958ea
Anuj Mathur <anuj.mathur@rewardle.com>
2021-03-17 17:20:41 +0000

    implemented redux saga advance

--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -1,10 +1,14 @@
-import { delay, put } from "redux-saga/effects";
+import { call, delay, put } from "redux-saga/effects";
 import axios from "axios";
 import * as actions from "../actions/index";
 export function* logoutSaga(action) {
-  yield localStorage.removeItem("token");
-  yield localStorage.removeItem("expirationDate");
-  yield localStorage.removeItem("userId");
+  yield call([localStorage,'removeItem'],'token')
+  yield call([localStorage,'removeItem'],'expirationDate')
+  yield call([localStorage,'removeItem'],'userId')
+ 
+  // yield localStorage.removeItem("token");
+  // yield localStorage.removeItem("expirationDate");
+  // yield localStorage.removeItem("userId");
   yield put(actions.logoutSucceed());
 }
 export function* checkAuthTimeoutSaga(action) {
--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,4 +1,5 @@
-import { takeEvery } from "redux-saga/effects";
+
+import { takeEvery,all, takeLatest } from "redux-saga/effects";
 import * as actionTypes from "../actions/actionTypes";
 import {
   authCheckStateSaga,
@@ -9,15 +10,21 @@ import {
 import { initIngredientsSaga } from "./burgerBuilder";
 import { fetchOrdersSaga, purchaseBurgerSaga } from "./order";
 export function* watchAuth() {
-  yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
-  yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
-  yield takeEvery(actionTypes.AUTH_USER, authUserSaga);
-  yield takeEvery(actionTypes.AUTH_CHECK_INITIATE_STATE, authCheckStateSaga);
+  yield all([
+  takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga),
+   takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga),
+   takeEvery(actionTypes.AUTH_USER, authUserSaga),
+   takeEvery(actionTypes.AUTH_CHECK_INITIATE_STATE, authCheckStateSaga),
+  ])
+  // yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
+  // yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
+  // yield takeEvery(actionTypes.AUTH_USER, authUserSaga);
+  // yield takeEvery(actionTypes.AUTH_CHECK_INITIATE_STATE, authCheckStateSaga);
 }
 export function* watchBurgerBuilder() {
   yield takeEvery(actionTypes.INIT_INGREDIENTS, initIngredientsSaga);
 }
 export function* watchOrder() {
-  yield takeEvery(actionTypes.PURCHASE_BURGER, purchaseBurgerSaga);
+  yield takeLatest(actionTypes.PURCHASE_BURGER, purchaseBurgerSaga);
   yield takeEvery(actionTypes.FETCH_ORDERS, fetchOrdersSaga);
 }
